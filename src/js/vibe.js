31e9a868506578f1219064c38d1978a4645b3f35
Vadym Moiseyenko <vadym.moiseyenko@agiliway.com>
2020-04-19 22:44:54 +0300

    init commit

---
+++ src/js/vibe.js
@@ -0,0 +1,6 @@
+const vibes = ["...Hi there", "...You are awesome", "...Wow", "...Amazing"];
+
+const vibe = vibes[Math.floor(Math.random() * vibes.length)];
+
+document.querySelector(".vibe").append(vibe);
+console.log(randomText);
