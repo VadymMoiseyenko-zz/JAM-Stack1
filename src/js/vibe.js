4cd503dfdd45839d8364fec8226c80ee32e45d9d
Vadym Moiseyenko <vadym.moiseyenko@agiliway.com>
2020-04-20 15:36:37 +0300

    Add external API

--- src/js/vibe.js
+++ src/js/vibe.js
@@ -3,4 +3,3 @@ const vibes = ["...Hi there", "...You are awesome", "...Wow", "...Amazing"];
 const vibe = vibes[Math.floor(Math.random() * vibes.length)];
 
 document.querySelector(".vibe").append(vibe);
-console.log(randomText);
