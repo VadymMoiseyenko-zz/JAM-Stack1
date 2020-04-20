90e8a188b17f0e9cd54c4502295ca461a75ad29b
Vadym Moiseyenko <vadym.moiseyenko@agiliway.com>
2020-04-20 23:12:46 +0300

    Add localization

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,11 +1,24 @@
 const axios = require("axios");
 require('dotenv').config();
+const countries = require("./countries.json")
 
-module.exports = async function(){
+
+ async function getNews(country){
    try{
-      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=ua&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
-      return response.data
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
+      return {
+         country,
+         articles: response.data.articles,
+      }
    } catch(e){
       console.log("wow");
    }
+}
+
+module.exports = async function(){
+   let newsPromises = countries.map(getNews);
+   return Promise.all(newsPromises).then(newsObject => {
+      console.log(newsObject)
+       return [].concat.apply([], newsObject)
+   } )
 }
